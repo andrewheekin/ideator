b3667ccbfd358fbf6808a8fdb0a9fae4a24273bf
Andrew Heekin <andrewheekin@gmail.com>
2020-09-22 23:34:07 -0400

    chat working

--- server/server.js
+++ server/server.js
@@ -1,29 +1,27 @@
-// TODO: change to -> import { GraphQLServer } from 'graphql-yoga';
-// and add in the babel preset to do this
-
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 const messages = [];
 
-// ! means field is reqired
-// make a Query type to get these messages
-// a Mutation type is like a POST in the Rest world
 const typeDefs = `
   type Message {
     id: ID!
     user: String!
     content: String!
   }
-
   type Query {
     messages: [Message!]
   }
-
   type Mutation {
     postMessage(user: String!, content: String!): ID!
   }
+  type Subscription {
+    messages: [Message!]
+  }
 `;
-// get the data defined in the type definitions with the resolvers
+
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
+
 const resolvers = {
   Query: {
     messages: () => messages,
@@ -36,12 +34,24 @@ const resolvers = {
         user,
         content,
       });
+      subscribers.forEach((fn) => fn());
       return id;
+    },
+  },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15);
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+        setTimeout(() => pubsub.publish(channel, { messages }), 0);
+        return pubsub.asyncIterator(channel);
+      },
     },
   },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 server.start(({ port }) => {
   console.log(`Server on http://localhost:${port}/`);
 });

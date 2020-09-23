1a2b0027af539b1534c9e1bba5c686e5eb8b8c50
Andrew Heekin <andrewheekin@gmail.com>
2020-09-22 22:36:53 -0400

    add apollo

--- ideator-client/src/App.js
+++ ideator-client/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import Chat from './Chat';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Chat />
         <a
           className="App-link"
           href="https://reactjs.org"
